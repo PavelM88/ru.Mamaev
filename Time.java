654021707c8d75694ec787d74d025b9b020a92ee
Pavel Mamaev <mamai_88@mail.ru>
2019-09-05 14:01:29 +0300

    Update DZ

--- Time.java
+++ Time.java
@@ -2,13 +2,13 @@ package task1;
 
 public class Time {
     int sec; // переменная секунды
-    int min; // переменная минуты
+    int hour; // переменная минуты
 
    class TimeTest1 {
-int convertacia = sec / (min * 2);
+int convertacia = sec /hour;
    }
 
     public static void main(String[] args) {
-        System.out.println(3600 / 60 / 60 + " час"); // переводим секунды в часы
+        System.out.println(3600 /3600 + " час"); // переводим секунды в часы
     }
 }
