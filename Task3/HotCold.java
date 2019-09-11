12d30b8e4407131a547f8db2ed4822a9c8f5fd66
Pavel Mamaev <mamai_88@mail.ru>
2019-09-11 17:33:35 +0300

    Исправил баг

--- Task3/HotCold.java
+++ Task3/HotCold.java
@@ -10,25 +10,25 @@ public class HotCold {
         int thisAttempt = 0;
 
         randomNumber = new Random().nextInt(100) +1;
-        System.out.println(randomNumber);
         do {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Введите число от 1 до 100 или 0 для выхода:");
             enterNumber = scanner.nextInt();
+            if (enterNumber == 0) {
+                System.out.println("Выход");
+                break;
+            }
 
             if (enterNumber == randomNumber) {
                 System.out.println("Вы угадали");
                 break;
             }
-
             prevAttempt = thisAttempt;
             thisAttempt = Math.abs(enterNumber - randomNumber);
 
             if (prevAttempt >= thisAttempt) {
                 System.out.println("Горячо");
-            }
-
-            else {
+            } else {
                 System.out.println("Холодно");
             }
         } while (enterNumber != 0);
