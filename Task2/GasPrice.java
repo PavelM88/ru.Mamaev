4c774b9e40c2404784d05dee5cf356cd7a660320
Pavel Mamaev <mamai_88@mail.ru>
2019-09-11 18:01:38 +0300

    home work2

---
+++ Task2/GasPrice.java
@@ -0,0 +1,15 @@
+package Task2;
+
+public class GasPrice {
+    int GasPrice = 43; // цена 1 литра бензина
+    int GasCount = 50; // нужное количество бензина
+
+  static int ymnozenie(int GasPrice, int GasCount) {
+
+      return GasPrice*GasCount;
+  }
+
+    public static void main(String[] args) {
+        System.out.println("Конечная стоимость бензина " + "= " + ymnozenie(43,50) + " руб");
+    }
+}
---
+++ Task2/Time.java
@@ -0,0 +1,10 @@
+package Task2;
+
+public class Time {
+
+    public static void main(String[] args) {
+        int sec; // переменная секунды
+        int hour; // переменная минуты
+        System.out.println(3600 /3600 + " час"); // переводим секунды в часы
+    }
+}
