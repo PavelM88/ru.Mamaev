65d713381ddc8e4e3d561d75cf01b64613fc79d2
Pavel Mamaev <mamai_88@mail.ru>
2019-10-01 16:42:58 +0300

    Task 11.Регистронезависимая замена слова.

--- Task11/Censorship.java
+++ Task11/Censorship.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 public class Censorship {
 
     public static void main(String[] args) {
-//     Scanner scanner = new Scanner(System.in);
-//     String keyboard = scanner.nextLine();
-//     System.out.println(keyboard.replaceAll("бяка","вырезано цензурой"));
         System.out.print("Введите сообщение: ");
         new Censorship().censor();
     }
@@ -15,6 +12,6 @@ public class Censorship {
     private void censor(){
         Scanner scanner1 = new Scanner(System.in);
         String keyboard1 = scanner1.nextLine();
-        System.out.println(keyboard1.replaceAll("^бяка","вырезано цензурой"));
+        System.out.println(keyboard1.replaceAll("(?iu)бяка","вырезано цензурой"));
     }
 }
