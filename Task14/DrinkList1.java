f238a147838b988b6205d11869e3b7b6e66061e0
Pavel Mamaev <mamai_88@mail.ru>
2019-10-09 10:26:16 +0300

    Task 14.

---
+++ Task14/DrinkList1.java
@@ -0,0 +1,24 @@
+package Task14;
+
+public enum DrinkList1 {
+    CAPPUCCINO(120,"RUB"),
+    ESPRESSO(100,"RUB" ),
+    AMERICANO(110,"RUB"),
+    LATTE(130,"RUB");
+    private int price;
+    private String currency;
+    DrinkList1(int price, String currency) {
+        this.price = price;
+        this.currency = currency;
+
+    }
+
+    public int getPrice () {
+
+        return price;
+    }
+    public String getCurrency () {
+
+        return currency;
+    }
+}
---
+++ Task14/VendingMachine1.java
@@ -0,0 +1,59 @@
+package Task14;
+
+import Task7.DrinkList;
+import org.apache.log4j.Logger;
+
+import java.util.Scanner;
+
+  class VendingMachine1 {
+    private final static Logger logger = Logger.getLogger(VendingMachine1.class);
+    public static void main(String[] args) {
+        logger.info("Запуск кофемашины");
+        System.out.println("     *** МЕНЮ ***");
+        for (DrinkList drinkList  : DrinkList.values()){
+            System.out.println(drinkList + " - " + drinkList.getPrice() + drinkList.getCurrency());
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Внесите сумму для выбора напитка:");
+        int enterAmount = scanner.nextInt();
+        if (enterAmount > 0) {
+        System.out.println("У вас на балансе: " + enterAmount + " RUB");
+        System.out.println("Выберете напиток: ");
+        System.out.println("Введите 1 для выбора CAPPUCCINO");
+        System.out.println("Введите 2 для выбора ESPRESSO");
+        System.out.println("Введите 3 для выбора AMERICANO");
+        System.out.println("Введите 4 для выбора LATTE");
+        System.out.println("Введите 0 для отмены");
+        } else {
+
+            System.out.println("Вы не внесли деньги");
+            logger.error("Нет денег на счете");
+            return;
+        }
+
+        int action = scanner.nextInt();
+
+        switch (action) {
+            case (1) : getDrink (enterAmount, DrinkList.CAPPUCCINO);
+                break;
+            case (2) : getDrink (enterAmount, DrinkList.ESPRESSO);
+                break;
+            case (3) : getDrink (enterAmount, DrinkList.AMERICANO);
+                break;
+            case (4) : getDrink (enterAmount, DrinkList.LATTE);
+                break;
+            case (0) : break;
+            default:
+                System.out.println("Вы выбрали несуществующий напиток");
+        }
+    }
+    private static void getDrink(int enterAmount, DrinkList menu) {
+        if (enterAmount >= menu.getPrice()) {
+            System.out.println("Ваш кофе готов");
+        } else {
+            System.out.println("У вас недостаточно средств");
+        }
+    }
+}
+
