1be070c27ac4db3d2dbe211f2459964dfdd56417
Pavel Mamaev <mamai_88@mail.ru>
2019-10-16 14:52:55 +0300

    Task 18. Кодировка файла.

--- Task18/EncodingFile.java
+++ Task18/EncodingFile.java
@@ -4,9 +4,17 @@ import java.io.*;
 import java.nio.charset.Charset;
 
 class EncodingFile {
+    void  creatFile () throws IOException {
+        File file =new File("Test");
+        file.mkdir();
+        File file1 = new File("Test/Textinput.txt");
+        file1.createNewFile();
+        Writer writer = new BufferedWriter(new FileWriter("Test/Text.txt"));
+        writer.write("Написать программу, которая копирует файл с одной кодировкой в файл с другой.\n");
+        writer.close();
+    }
     void convert() throws IOException {
         File fileInput = new File("C:\\Users\\paxat\\IdeaProjects\\ru.Mamaev\\Test\\Textinput.txt");
-        fileInput.mkdirs();
         try(Writer writer = new OutputStreamWriter(new FileOutputStream(fileInput), Charset.forName("cp866"))) {
             FileReader fileReader = new FileReader("C:\\Users\\paxat\\IdeaProjects\\ru.Mamaev\\Test\\Text.txt");
             int a;
--- Task18/TestEncodingFile.java
+++ Task18/TestEncodingFile.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class TestEncodingFile {
     public static void main(String[] args) throws IOException {
         EncodingFile ef = new EncodingFile();
+        ef.creatFile();
         ef.convert();
     }
 }
