3229ef228da20c745a7ce0a3634826a12d927c07
Pavel Mamaev <mamai_88@mail.ru>
2019-10-21 15:09:10 +0300

    Task 19.

---
+++ Task19/TestCheck.java
@@ -0,0 +1,20 @@
+package Task19;
+
+import java.io.IOException;
+
+public class TestCheck {
+    public static void main(String[] args) throws IOException {
+    Check check = new Check();
+    check.checkInfo();
+    check.printCheck();
+//        Scanner scanner = new Scanner(new File("C:\\Users\\paxat\\IdeaProjects\\ru.Mamaev\\src\\Task19\\check"));
+//        while (scanner.hasNextLine()) {
+//            String s = scanner.next();
+//            if (scanner.hasNext()) {
+//                Float f = scanner.nextFloat();
+//                Float f1 = scanner.nextFloat();
+//                System.out.printf("%s %05.3f %05.2f \n", s, f, f1);
+//            }
+//        }
+    }
+}
