7d2fa8b854e6269ef9e410049883edc628b8851c
Pavel Mamaev <mamai_88@mail.ru>
2019-10-22 19:24:32 +0300

    Task 19.

--- Task19/Check.java
+++ Task19/Check.java
@@ -13,9 +13,12 @@ class Check {
                 Float f = input.nextFloat();
                 Float f1 = input.nextFloat();
                 Float f2 = f1 * f;
-                System.out.printf("%-16s %8.2f x %7.3f  %10.2f\n", line, f1, f, f2);
+                System.out.printf("%-16s %8.2f x %7.3f  =%10.2f\n", line, f1, f, f2);
+                if (input.hasNext())
                 input.nextLine();
             }
+            System.out.println("================================================");
+            System.out.print("Итого: ");
             input.close();
     }
 
@@ -24,8 +27,8 @@ class Check {
         String s2 = "Цена";
         String s3 = "Кол-во";
         String s4 = "Стоимость";
-        System.out.printf("%s  %10s %11s %10s \n", s1, s2, s3, s4);
-        System.out.println("===============================================");
+        System.out.printf("%s  %10s %11s %11s \n", s1, s2, s3, s4);
+        System.out.println("================================================");
     }
 }
 
