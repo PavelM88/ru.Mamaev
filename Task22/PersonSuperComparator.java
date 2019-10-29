dd4f96e7e0d787e16d649e19872ec89d1a8e300f
Pavel Mamaev <mamai_88@mail.ru>
2019-10-29 16:52:21 +0300

    Task22. Comparator

---
+++ Task22/PersonSuperComparator.java
@@ -0,0 +1,19 @@
+package Task22;
+
+import java.util.Comparator;
+
+public class PersonSuperComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person a, Person b) {
+        String p1 = a.getName();
+        String p2 = b.getName();
+        int sCompare = p1.compareTo(p2);
+        if (sCompare != 0){
+            return sCompare;
+        } else {
+            Integer i1 = a.getAge();
+            Integer i2 = b.getAge();
+            return i1.compareTo(i2);
+        }
+    }
+}
---
+++ Task22/TestComparator.java
@@ -0,0 +1,21 @@
+package Task22;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestComparator {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Борис", 40));
+        people.add(new Person("Александр", 35));
+        people.add(new Person("Алиса", 30));
+        people.add(new Person("Федор",28));
+        people.add(new Person("Александр", 25));
+
+        Collections.sort(people, new PersonSuperComparator());
+        for (Person person : people) {
+            System.out.println(person.getName() + " " + person.getAge());
+        }
+    }
+}
