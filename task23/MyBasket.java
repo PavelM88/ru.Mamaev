c351a29fbaeaadf2c209922ea013214936bef6c9
Pavel Mamaev <mamai_88@mail.ru>
2019-11-06 21:41:58 +0300

    task23. Корзина интернет магазина

--- task23/MyBasket.java
+++ task23/MyBasket.java
@@ -1,47 +1,39 @@
 package task23;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyBasket implements Basket{
 
-    private static final List<String> basketList = new ArrayList<>();
-    private String product;
-    private int quantity;
+    private static final List<Product> basketList = new ArrayList<>();
 
-    public String getProduct() {
-        return product;
-    }
-
-    public void setProduct(String product) {
-        this.product = product;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
 
     @Override // добавить продукт
     public void addProduct(String product, int quantity) {
-        basketList.add(product);
-        basketList.add(String.valueOf(quantity));
-        System.out.println("В корзину добавлено: " + product + " кол-во: " + quantity);
+        basketList.add(new Product(product, quantity));
+        System.out.println("В корзину добавлено: [" + product + "] кол-во: [" + quantity + "]");
 
     }
 
     @Override // удалить продукт
     public void removeProduct(String product) {
-        basketList.remove(product);
-        basketList.remove(quantity);
+        Iterator<Product> iterator = basketList.iterator();
+        if (iterator.hasNext()) {
+            if (iterator.next().getProduct().equals(product)) {
+                iterator.remove();
+            }
+        }
     }
 
     @Override // обновить кол-во продукта
     public void updateProductQuantity(String product, int quantity) {
-
+        for (Product prod: basketList) {
+            if (prod.getProduct().equals(product)) {
+                prod.setQuantity(quantity);
+            }
+        }
     }
 
     @Override // очистить корзину
@@ -51,12 +43,18 @@ public class MyBasket implements Basket{
 
     @Override // какие продукты в корзине
     public List<String> getProducts() {
-        System.out.println(basketList); // выводить продукт и кол-во. Нужно вывести только продукт
-        return null;
+        List<String> collect = basketList.stream().map(r -> r.getProduct()).collect(Collectors.toList());
+        System.out.println(collect);
+        return collect;
     }
 
     @Override
     public int getProductQuantity(String product) {
+        for (Product prod : basketList) {
+            if (prod.getProduct().equals(product)) {
+                System.out.println("В корзине: " + product + " кол-во: " + prod.getQuantity());
+            }
+        }
         return 0;
     }
 
