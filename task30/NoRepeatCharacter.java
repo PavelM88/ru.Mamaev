427e3ddb73e6033d46f35fd33961edf889f66213
Pavel Mamaev <mamai_88@mail.ru>
2019-11-21 17:27:17 +0300

    task30. Первый неповторяющийся символ.

---
+++ task30/NoRepeatCharacter.java
@@ -0,0 +1,31 @@
+package task30;
+
+import java.util.Scanner;
+
+public class NoRepeatCharacter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите слово: ");
+        String word = scanner.nextLine();
+        getFirstUniqueChar(word);
+        readFirstNonRepeatingChar(word);
+    }
+
+    private static void readFirstNonRepeatingChar(String s) {
+        System.out.print("Первый неповторяющийся символ: ");
+        System.out.println(getFirstUniqueChar(s));
+    }
+
+
+    private static String getFirstUniqueChar(String line) {
+        String result = null;
+        for (int i = 0; i < line.length(); i++) {
+            char currentChar = line.charAt(i);
+            if (line.indexOf(currentChar) == line.lastIndexOf(currentChar)) {
+                result = Character.toString(currentChar);
+                break;
+            }
+        }
+        return result;
+    }
+}
